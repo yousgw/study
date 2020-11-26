d7d75d968bd1b6a6f186144d5be5eeedd9e6ba18
yousgw <r115322@g.kobe-kosen.ac.jp>
2020-11-27 01:03:01 +0900

    vue-cliによるプロジェクトを作成。vueファイルの書き方を学習。

---
+++ vue.js/test/src/components/Test.vue
@@ -0,0 +1,27 @@
+<template>
+    <div id="app">
+        <input v-model="message">
+        <button v-on:click="add">add</button>
+        <ol>
+            <li v-for="todo in todos">
+                {{ todo.text}}
+            </li>
+        </ol>
+    </div>
+</template>
+
+<script>
+    export default {
+        el: '#app',
+        data () {
+            return {
+                todos: []
+            }
+        },
+        methods: {
+            add: function () {
+            this.todos.push({ text: this.message})
+            }
+        }
+    }
+</script>
